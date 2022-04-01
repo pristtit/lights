b86979db3d19df9b19276675716b7e6c27f5422b
pristtit <pristtit@mail.ru>
2022-04-01 20:30:32 +0300

    deploy

---
+++ js/chunk-2d0bd795.40056c44.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bd795"],{"2bc2":function(n,t,e){"use strict";e.r(t);var o=e("7a23");function c(n,t,e,c,r,u){return Object(o["p"])(),Object(o["d"])("h1",null,"GG")}var r={data:function(){return{nam:"dima",t:500}},mounted:function(){t.bind(this);var n=setInterval((function(){return console.log(500)}),this.t);function t(){alert(this.nam)}setTimeout((function(){clearInterval(n),console.log(1)}),3e3)}},u=e("6b0d"),a=e.n(u);const i=a()(r,[["render",c]]);t["default"]=i}}]);
+//# sourceMappingURL=chunk-2d0bd795.40056c44.js.map
